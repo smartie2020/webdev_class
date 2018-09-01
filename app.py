931b6a0deadcf5d4c9b91640a50a2f93e64f32da
smartie <smartie2020@gmail.com>
2018-09-01 11:22:37 -0500

    Main Method

--- app.py
+++ app.py
@@ -20,6 +20,6 @@ def submit():
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 #run the application
-if __name__: '__main__':
-port = 8000
-app.run(host="0.0.0.0", port=port, debug=True)
+if __name__ == '__main__':
+    port=8000
+    app.run(host="0.0.0.0", port=port, debug=True)
